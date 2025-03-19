1e77ec73d95b605716fd2a08b62b9b079a4a5bdf
akyoshid <akyoshid@student.42tokyo.jp>
2025-03-19 14:47:46 +0000

    [ADD] main.c, minishell.h, libft & Makefile

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME =		minishell
+
+INC_DIR =	inc/
+SRC_DIR =	src/
+OBJ_DIR =	obj/
+LIBFT_DIR =	libft/
+
+INC =				$(INC_DIR)minishell.h
+
+SRC = \
+					$(SRC_DIR)main.c
+
+OBJ =				$(patsubst $(SRC_DIR)%.c,$(OBJ_DIR)%.o,$(SRC))
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+LIBFT_LIB = -L ./libft -lft -lreadline
+
+RM = rm -f
+AR = ar rcs
+
+all: $(NAME)
+
+bonus: $(NAME)
+
+$(NAME): $(OBJ)
+	@make -C $(LIBFT_DIR)
+	$(CC) $(CFLAGS) $(OBJ) -I$(INC_DIR) -o $@ $(LIBFT_LIB)
+
+$(OBJ_DIR)%.o: $(SRC_DIR)%.c $(INC)
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -I$(INC_DIR) -c $< -o $@
+
+clean:
+	$(RM) -r $(OBJ_DIR)
+	@make clean -C $(LIBFT_DIR)
+
+fclean: clean
+	$(RM) $(NAME) $(BONUS_NAME)
+	@make fclean -C $(LIBFT_DIR)
+
+re: fclean all
+
+.PHONY: all clean fclean re bonus
---
+++ inc/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 13:11:26 by akyoshid          #+#    #+#             */
+/*   Updated: 2025/03/19 13:57:54 by akyoshid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <signal.h>
+# include <string.h>
+# include <dirent.h>
+# include <termios.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
