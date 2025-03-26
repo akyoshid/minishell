1d1ab60643b8e2f20d9a49a6af305f529f91bffa
akyoshid <akyoshid@student.42tokyo.jp>
2025-03-26 18:10:29 +0000

    [FIX] fatal error in create_env_node()

--- src/env/create_env_x.c
+++ src/env/create_env_x.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:59:41 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/03/26 13:34:01 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/03/26 18:09:06 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_env_list	*create_env_node(char *key, char *value)
 {
 	t_env	*content;
 
-	if (ft_strchr(value, '\'') != NULL && ft_strchr(value, '\"') != NULL)
+	if (ft_strchr(value, '\'') != NULL || ft_strchr(value, '\"') != NULL)
 	{
 		ft_dprintf(STDERR_FILENO,
 			"minishell: init_env_list: `%s=%s': "
