1ab952a94ed4ea3ad3609eccd07e3140e0ad317b
akyoshid <akyoshid@student.42tokyo.jp>
2025-04-14 02:41:20 +0000

    [FIX] not output '_' in export command

--- src/builtin/export_no_operand.c
+++ src/builtin/export_no_operand.c
@@ -6,7 +6,7 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 19:50:29 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/04/12 19:51:12 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/04/14 02:33:52 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,21 @@ void	export_no_operand(t_ctx *ctx)
 {
 	t_env_list	*env_list_dup;
 	t_env_list	*current_node;
+	char		*key;
+	char		*value;
 
 	env_list_dup = dup_env_list(ctx->env_list);
 	env_list_dup = sort_env_list(env_list_dup);
 	current_node = env_list_dup;
 	while (current_node != NULL)
 	{
-		printf("declare -x %s=", get_env_key(current_node));
-		printf("\"%s\"\n", get_env_value(current_node));
+		key = get_env_key(current_node);
+		value = get_env_value(current_node);
+		if (ft_strcmp(key, "_") != 0)
+		{
+			printf("declare -x %s=", key);
+			printf("\"%s\"\n", value);
+		}
 		current_node = current_node->next;
 	}
 	clear_env_list(&env_list_dup);
